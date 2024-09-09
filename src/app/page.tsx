38ad6360e49b1a9ee3999a838a6b859821863296
Key Cayetano <kma.cayetano@gmail.com>
2024-09-08 21:58:52 -0400

    added static image

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,7 +5,7 @@ export default function Home() {
   return (
     <div className={styles.page}>
       <main className={styles.main}>
-        <Image src="/servgenie-logo.png" alt="Servgenie Logo"></Image>
+        <Image src="/servgenie-logo.png" alt="Servgenie Logo" width={100} height={150}></Image>
         <h1>Any wish, anytime, anywhere.</h1>
         <p><i>Coming Soon...</i></p>
       </main>
